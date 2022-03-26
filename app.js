6880c80573b2169b3baa14a48624f2a9f9095838
unknown <fajarmh2@gmail.com>
2022-03-26 11:46:55 +0700

    First commit & have created post and get

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+//TODO
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", (req, res) => {
+    Article.find({}, (err, result) => {
+        if(!err) {
+            res.send(result);
+        } else {
+            res.send(err);
+        }
+    })
+});
+
+app.post("/articles", (req, res) => {
+    console.log(req.body);
+    console.log(req.body.title);
+    console.log(req.body.content);
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save((err) => {
+        if(err) {
+            res.send(err);
+        } else {
+            res.send("Sucessfully added a new article!");
+        }
+    });
+});
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on http://localhost:3000/");
+});
