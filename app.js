d74ec73309c9549158f7d8d257d02296a20ddafd
unknown <fajarmh2@gmail.com>
2022-03-26 12:03:34 +0700

    Refactoring post,delete,get

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(express.static("public"));
 
@@ -22,47 +22,46 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", (req, res) => {
-    Article.find({}, (err, result) => {
-        if(!err) {
-            res.send(result);
-        } else {
-            res.send(err);
-        }
+app.route("/articles")
+    .get((req, res) => {
+        Article.find({}, (err, result) => {
+            if (!err) {
+                res.send(result);
+            } else {
+                res.send(err);
+            }
+        })
     })
-});
 
-app.post("/articles", (req, res) => {
-    console.log(req.body);
-    console.log(req.body.title);
-    console.log(req.body.content);
+    .post((req, res) => {
+        console.log(req.body);
+        console.log(req.body.title);
+        console.log(req.body.content);
 
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+
+        newArticle.save((err) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send("Sucessfully added a new article!");
+            }
+        });
+    })
+
+    .delete((req, res) => {
+        Article.deleteMany({}, (err) => {
+            if (!err) {
+                res.send("Sucessfully delete all articles!");
+            } else {
+                res.send(err)
+            }
+        });
     });
 
-    newArticle.save((err) => {
-        if(err) {
-            res.send(err);
-        } else {
-            res.send("Sucessfully added a new article!");
-        }
-    });
-});
-
-
-app.delete("/articles", (req, res) => {
-    Article.deleteMany({}, (err) => {
-        if(!err) {
-            res.send("Sucessfully delete all articles!");
-        } else {
-            res.send(err)
-        }
-    });
-});
-
-
-app.listen(3000, function() {
-  console.log("Server started on http://localhost:3000/");
+app.listen(3000, function () {
+    console.log("Server started on http://localhost:3000/");
 });
